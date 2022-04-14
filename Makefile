a5d40f163e8c0062890263b51de575601d51f250
alpatakan <atakanisbakan@gmail.com>
2022-04-15 00:21:30 +0300

    SDL support for key input

--- Makefile
+++ Makefile
@@ -3,8 +3,8 @@ SRCS=sin-synth.c
 OBJS=$(SRCS:.c=.o)
 
 CC=gcc
-CFLAGS=-I/usr/lib/include/spa-0.2 -I/usr/lib/include/pipewire-0.3
-LDFLAGS=-L/usr/lib/lib/x86_64-linux-gnu -lpipewire-0.3 -lm -Wl,-rpath=/usr/lib/lib/x86_64-linux-gnu
+CFLAGS=-I/usr/lib/include/spa-0.2 -I/usr/lib/include/pipewire-0.3 -I/usr/include/SDL2 -D_REENTRANT
+LDFLAGS=-L/usr/lib/lib/x86_64-linux-gnu -lpipewire-0.3 -lm -lSDL2 -Wl,-rpath=/usr/lib/lib/x86_64-linux-gnu
 
 $(TARGET): $(OBJS)
 	$(CC) -o $@ $^ $(LDFLAGS)
--- sin-synth.c
+++ sin-synth.c
@@ -3,15 +3,12 @@
 #include <math.h>
 #include <signal.h>
 
-#include <termios.h>
-#include <unistd.h>
+#include "SDL.h"
 
 #include <spa/param/audio/format-utils.h>
-
 #include <pipewire/pipewire.h>
 
 #define M_PI_M2 (M_PI + M_PI)
-
 #define DEFAULT_RATE 44100
 #define DEFAULT_CHANNELS 2
 #define DEFAULT_VOLUME 0.7
@@ -30,21 +27,33 @@
 #define B4 493.88
 #define C5 523.25
 
-struct data
-{
-    struct pw_main_loop *loop;
-    struct pw_stream *stream;
+typedef uint8_t Boolean;
 
+typedef struct state {
+    Boolean quit;
+    Boolean play;
+    float note;
+} state_t;
+
+/* TODO lock this */
+static state_t state = {
+    .quit = 0,
+    .play = 0,
+    .note = 0
+};
+
+struct data {
+    struct pw_main_loop* loop;
+    struct pw_stream* stream;
     double accumulator;
 };
 
-static void fill_f32(struct data *d, void *dest, int n_frames, float note)
+static void fill_f32(struct data* d, void* dest, int n_frames, float note)
 {
     float *dst = dest, val;
     int i, c;
 
-    for (i = 0; i < n_frames; i++)
-    {
+    for (i = 0; i < n_frames; i++) {
         d->accumulator += M_PI_M2 * note / DEFAULT_RATE;
         if (d->accumulator >= M_PI_M2)
             d->accumulator -= M_PI_M2;
@@ -55,61 +64,57 @@ static void fill_f32(struct data *d, void *dest, int n_frames, float note)
     }
 }
 
-static float get_note(void)
+static void set_note(SDL_Keysym * sym)
 {
-    float note = 0;
-    int in = getchar();
-
-    switch (in)
+    printf("key press %s\n", SDL_GetKeyName(sym->sym));
+    switch (sym->sym)
     {
     case 'a':
-        note = C4;
+        printf("a\n");
+        state.note = C4;
         break;
     case 'w':
-        note = Db4;
+        state.note = Db4;
         break;
     case 's':
-        note = D4;
+        state.note = D4;
         break;
     case 'e':
-        note = Eb4;
+        state.note = Eb4;
         break;
     case 'd':
-        note = E4;
+        state.note = E4;
         break;
     case 'f':
-        note = F4;
+        state.note = F4;
         break;
     case 't':
-        note = Gb4;
+        state.note = Gb4;
         break;
     case 'g':
-        note = G4;
+        state.note = G4;
         break;
     case 'y':
-        note = Ab4;
+        state.note = Ab4;
         break;
     case 'h':
-        note = A4;
+        state.note = A4;
         break;
     case 'u':
-        note = Bb4;
+        state.note = Bb4;
         break;
     case 'j':
-        note = B4;
+        state.note = B4;
         break;
     case 'k':
-        note = C5;
+        state.note = C5;
         break;
     case 'q':
-        note = -1;
+        state.note = 0;
         break;
     default:
         break;
     }
-
-    putchar(0x08);
-    return note;
 }
 
 static void on_process(void *userdata)
@@ -118,21 +123,18 @@ static void on_process(void *userdata)
     struct pw_buffer *b;
     struct spa_buffer *buf;
     int n_frames, stride;
-    uint8_t *p;
-    float note = get_note();
+    uint8_t* p;
 
-    if (note == -1)
-    {
+    if (state.quit) {
         pw_main_loop_quit(data->loop);
         return;
     }
-    if (note == 0)
-    {
+
+    if (!state.play) {
         return;
     }
 
-    if ((b = pw_stream_dequeue_buffer(data->stream)) == NULL)
-    {
+    if ((b = pw_stream_dequeue_buffer(data->stream)) == NULL) {
         pw_log_warn("out of buffers: %m");
         return;
     }
@@ -144,7 +146,7 @@ static void on_process(void *userdata)
     stride = sizeof(float) * DEFAULT_CHANNELS;
     n_frames = buf->datas[0].maxsize / (stride * 2);
 
-    fill_f32(data, p, n_frames, note);
+    fill_f32(data, p, n_frames, state.note);
 
     buf->datas[0].chunk->offset = 0;
     buf->datas[0].chunk->stride = stride;
@@ -158,47 +160,31 @@ static const struct pw_stream_events stream_events = {
     .process = on_process,
 };
 
-int main(int argc, char *argv[])
+void pw_run(void)
 {
     struct data data = {
         0,
     };
-    const struct spa_pod *params[1];
+    const struct spa_pod* params[1];
     uint8_t buffer[1024];
     struct spa_pod_builder b = SPA_POD_BUILDER_INIT(buffer, sizeof(buffer));
-    static struct termios oldt, newt;
 
-    tcgetattr(STDIN_FILENO, &oldt);
-    newt = oldt;
-    newt.c_lflag &= ~(ICANON);
-    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-
-    pw_init(&argc, &argv);
     data.loop = pw_main_loop_new(NULL);
     data.stream = pw_stream_new_simple(
-        pw_main_loop_get_loop(data.loop),
-        "audio-src",
-        pw_properties_new(
-            PW_KEY_MEDIA_TYPE, "Audio",
-            PW_KEY_MEDIA_CATEGORY, "Playback",
-            PW_KEY_MEDIA_ROLE, "Music",
-            NULL),
-        &stream_events,
-        &data);
+        pw_main_loop_get_loop(data.loop), "audio-src",
+        pw_properties_new(PW_KEY_MEDIA_TYPE, "Audio", PW_KEY_MEDIA_CATEGORY,
+            "Playback", PW_KEY_MEDIA_ROLE, "Music", NULL),
+        &stream_events, &data);
 
-    params[0] = spa_format_audio_raw_build(&b, SPA_PARAM_EnumFormat,
-                                           &SPA_AUDIO_INFO_RAW_INIT(
-                                                   .format = SPA_AUDIO_FORMAT_F32,
-                                                   .channels = DEFAULT_CHANNELS,
-                                                   .rate = DEFAULT_RATE));
+    params[0] = spa_format_audio_raw_build(
+        &b, SPA_PARAM_EnumFormat,
+        &SPA_AUDIO_INFO_RAW_INIT(.format = SPA_AUDIO_FORMAT_F32,
+            .channels = DEFAULT_CHANNELS,
+            .rate = DEFAULT_RATE));
 
-    pw_stream_connect(data.stream,
-                      PW_DIRECTION_OUTPUT,
-                      argc > 1 ? (uint32_t)atoi(argv[1]) : PW_ID_ANY,
-                      PW_STREAM_FLAG_AUTOCONNECT |
-                          PW_STREAM_FLAG_MAP_BUFFERS |
-                          PW_STREAM_FLAG_RT_PROCESS,
-                      params, 1);
+    pw_stream_connect(data.stream, PW_DIRECTION_OUTPUT, PW_ID_ANY,
+        PW_STREAM_FLAG_AUTOCONNECT | PW_STREAM_FLAG_MAP_BUFFERS | PW_STREAM_FLAG_RT_PROCESS,
+        params, 1);
 
     pw_main_loop_run(data.loop);
 
@@ -206,5 +192,87 @@ int main(int argc, char *argv[])
     pw_main_loop_destroy(data.loop);
     pw_deinit();
 
+    return;
+}
+
+/* Very simple thread - counts 0 to 9 delaying 50ms between increments */
+static int SDLCALL input_thread(void* ptr)
+{
+    SDL_Event event;
+
+    while (!state.quit && SDL_WaitEvent(&event)) {
+        switch (event.type) {
+        case SDL_KEYDOWN:
+        case SDL_KEYUP:
+            if (event.key.repeat) {
+                break;
+            }
+            set_note(&event.key.keysym);
+            if (event.key.state == SDL_PRESSED) {
+                printf("play\n");
+                state.play = 1;
+            } else {
+                printf("stop\n");
+                state.play = 0;
+            }
+            break;
+        case SDL_MOUSEBUTTONDOWN:
+        case SDL_QUIT:
+            state.quit = 1;
+            break;
+        default:
+            break;
+        }
+    }
+
     return 0;
+}
+
+void sdl_run(void)
+{
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+    SDL_Thread* thread;
+
+    /* Initialize SDL */
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't initialize SDL: %s\n", SDL_GetError());
+        state.quit = 1;
+        return;
+    }
+
+    /* Set 640x480 video mode */
+    window = SDL_CreateWindow("CheckKeys Test",
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        640, 480, 0);
+
+    if (!window) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create 640x480 window: %s\n",
+            SDL_GetError());
+        state.quit = 1;
+        return;
+    }
+
+    // renderer = SDL_CreateRenderer(window, -1, 0);
+    // SDL_RenderPresent(renderer);
+
+    SDL_StartTextInput();
+    SDL_PumpEvents();
+
+    thread = SDL_CreateThread(input_thread, "InputThread", NULL);
+}
+
+int main(int argc, char* argv[])
+{
+    /* Init SDL and listen input events */
+    sdl_run();
+
+    /* Init pw */
+    pw_init(&argc, &argv);
+
+    /* Run pw stream for tone generation
+     * Blocks until pw loop finishes */
+    pw_run();
+
+    SDL_Quit();
 }
